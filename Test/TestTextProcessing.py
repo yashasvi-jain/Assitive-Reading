1054b382b92a8ee1c1a466c1cbe116be7e839006
yashasvi-jain <47085019+yashasvi-jain@users.noreply.github.com>
2021-10-27 13:17:15 -0400

    The word_tokenizer can now tokenize numerical values as words.

--- Test/TestTextProcessing.py
+++ Test/TestTextProcessing.py
@@ -18,5 +18,18 @@ class TestTextProcessing(unittest.TestCase):
         result = self._TextProcessing.word_tokenizer(sentence)
         self.assertEqual(result, ['hello', 'world'])
 
+    def test_word_tokenizer_for_a_sentence_with_numbers(self):
+        sentence1 = 'Hello World 42!'
+        result = self._TextProcessing.word_tokenizer(sentence1)
+        self.assertEqual(result, ['hello', 'world', 'forty', 'two'])
+
+        sentence2 = '10001'
+        result = self._TextProcessing.word_tokenizer(sentence2)
+        self.assertEqual(result, ['ten', 'thousand', 'and', 'one'])
+
+        sentence3 = 'I got 99 problems and money ain\'t one'
+        result = self._TextProcessing.word_tokenizer(sentence3)
+        self.assertEqual(result, ['i', 'got', 'ninety', 'nine', 'problems', 'and', 'money', 'ain\'t', 'one'])
+
 if __name__ == '__main__':
     unittest.main()
--- src/text_processing.py
+++ src/text_processing.py
@@ -1,21 +1,40 @@
-from nltk import sent_tokenize
-from nltk.tokenize import TreebankWordTokenizer
+from nltk import sent_tokenize as st
+from nltk import word_tokenize as wt
+from nltk.tokenize import WordPunctTokenizer
 import string
+from num2words import num2words
 
 class TextProcessing:
   def __init__(self):
     self._punctuations = string.punctuation
+    self._wpt = WordPunctTokenizer()
 
   def sentence_tokenizer(self, text):
-    tokenzied_paragraph = sent_tokenize(text, language='english')
+    tokenzied_paragraph = st(text, language='english')
     return tokenzied_paragraph
 
   def word_tokenizer(self, sentence):
-    sentence = sentence.lower()
-    tokenized_sentence = TreebankWordTokenizer().tokenize(sentence)
+    tokenized_sentence = self._wpt.tokenize(sentence)
+
+    for i in range( len(tokenized_sentence) ):
+      word = tokenized_sentence[i]
+
+      if word.isdigit():
+        tokenized_sentence.remove(word)
+        tokenized_number = self._wpt.tokenize(num2words(word))
+        for n in range( len(tokenized_number) ):
+          tokenized_sentence.insert( i+n, tokenized_number[n])
+        i = i + len(tokenized_number)
+
+      if (word == '\''):
+        tokenized_sentence[i-1] += '\'' + tokenized_sentence[i+1]
+        tokenized_sentence.pop(i)
+        tokenized_sentence.pop(i)
 
     for punctuation in self._punctuations:
       if punctuation in tokenized_sentence:
         tokenized_sentence.remove(punctuation)
+
+    tokenized_sentence = list(map(lambda x: x.lower(), tokenized_sentence))
 
     return tokenized_sentence
