2ed3ecb969474e93ba61bf4428a6c087e33c0d5d
yashasvi-jain <47085019+yashasvi-jain@users.noreply.github.com>
2022-07-08 18:58:15 -0400

    Added cases for catching missed and added words

--- helpers/utils.py
+++ helpers/utils.py
@@ -4,7 +4,7 @@ class Utils:
     def __init__(self):
         pass
 
-    def word_frequency(self, word: str, text: list) -> int:
-        # count = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(word), text))
-        # return count
+    def word_frequency(self, word: str, text) -> int:
+        if isinstance(text, str):
+            return sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(word), text))
         return text.count(word)
--- src/catch_mistake.py
+++ src/catch_mistake.py
@@ -10,30 +10,43 @@ class CatchMistake:
     self._valid_words = valid_words
     self._flag = 0
     self._words_were_missed = len(self._transcribed_audio) < len(self._valid_words)
-    self._words_were_added = len(self._transcribed_audio) > len(self._valid_words):
+    self._words_were_added = len(self._transcribed_audio) > len(self._valid_words)
     self._DM = DatabaseManager()
     self._Utils = Utils()
-    pass
-
-  # def checkIfWordsWereMissed(self):
-  #   i = len(self._transcribed_audio) - 1
-  #   j = len(self._valid_words) - 1
-  #   for
 
   def catch(self):
-    added_words_range = set() # (start index, end index)
 
     if self._words_were_missed:
-      pass
+      words_missed = []
+      words_missed_index = []
+      for valid_word in self._valid_words:
+        if valid_word not in self._transcribed_audio:
+          words_missed.append(valid_word)
+          words_missed_index.append(self._valid_words.index(valid_word))
+      # ADD DB STROE COMMAND
 
     elif self._words_were_added:
-      for i range()
+      words_added = self._transcribed_audio.copy()
+      for word in self._transcribed_audio:
+        if word in self._valid_words:
+          words_added.remove(word)
+      # ADD DB STROE COMMAND
 
     else:
       for i in range(len(self._transcribed_audio)):
         if self._transcribed_audio[i] != self._valid_words[i]:
-          this._flag = i
+          self._flag = i
           correct_word = self._valid_words[i]
-          wrong_word = elf._transcribed_audio[i]
+          wrong_word = self._transcribed_audio[i]
           total_count = self._Utils.word_frequency(correct_word, self._valid_words)
+
+          # Insert/Update row in WrongWords Table
           self._DM.addToWrongWords(correct_word, wrong_word, total_count)
+
+if __name__ == '__main__':
+  # ls1 = ['hello', 'i', 'am', 'student']
+  # ls2 = ['hello', 'i', 'am', 'teacher']
+
+  # CM = CatchMistake(ls1, ls2)
+  # CM.catch()
+  pass
